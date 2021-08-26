42fe85c0ed0a95c6fd34447b3d55ae9cbaa3b367
Rahmouni Abdelhak <noisyboy.96.84@gmail.com>
2021-08-26 15:25:00 +0100

    implement front end
    - Titles
    - Filter inputs
    - Tables

--- frontend/src/views/Main.js
+++ frontend/src/views/Main.js
@@ -1,4 +1,4 @@
-import { Container, Grid, Typography, Box } from "@material-ui/core";
+import { Container, Grid, Typography, TextField, Box } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import {
   Table,
@@ -16,16 +16,16 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(name, calories, fat, carbs) {
+  return { name, calories, fat, carbs };
 }
 
 const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24),
+  createData("Ice cream sandwich", 237, 9.0, 37),
+  createData("Eclair", 262, 16.0, 24),
+  createData("Cupcake", 305, 3.7, 67),
+  createData("Gingerbread", 356, 16.0, 49),
 ];
 
 function Main() {
@@ -35,24 +35,33 @@ function Main() {
     <Container maxWidth="md">
       <Grid
         container
-        spacing={3}
+        spacing={5}
         justifyContent="center"
         alignItems="stretch"
         direction="column"
       >
-        <Grid item lg={10}>
-          <Typography variant="h4" component="h4">
-            Top games by playtime
-          </Typography>
+        <Grid item lg={12}>
+          <Box my={2}>
+            <Typography variant="h4" component="h4">
+              Top games by playtime
+            </Typography>
+            <Box my={2} display="flex" justifyContent="flex-end">
+              <Box>
+                <TextField m={2} size="small" id="outlined-basic" label="Filter by genre" variant="outlined" />
+              </Box>
+              <Box ml={1}>
+                <TextField m={2} size="small" id="outlined-basic" label="Filter by platform" variant="outlined" />
+              </Box>
+            </Box>
+          </Box>
           <TableContainer component={Paper}>
             <Table className={classes.table} aria-label="simple table">
               <TableHead>
                 <TableRow>
-                  <TableCell>Dessert (100g serving)</TableCell>
-                  <TableCell align="right">Calories</TableCell>
-                  <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                  <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                  <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                  <TableCell>Games</TableCell>
+                  <TableCell align="right">Platforms</TableCell>
+                  <TableCell align="right">Genre</TableCell>
+                  <TableCell align="right">Total play time</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
@@ -71,19 +80,30 @@ function Main() {
             </Table>
           </TableContainer>
         </Grid>
-        <Grid item lg={10}>
-          <Typography variant="h4" component="h4">
-            Top games by number of players
-          </Typography>
+
+
+        <Grid item lg={12}>
+          <Box my={2}>
+            <Typography variant="h4" component="h4">
+              Top games by number of players
+            </Typography>
+            <Box my={2} display="flex" justifyContent="flex-end">
+              <Box>
+                <TextField size="small" id="outlined-basic" label="Filter by genre" variant="outlined" />
+              </Box>
+              <Box ml={1}>
+                <TextField size="small" id="outlined-basic" label="Filter by platform" variant="outlined" />
+              </Box>
+            </Box>
+          </Box>
           <TableContainer component={Paper}>
             <Table className={classes.table} aria-label="simple table">
               <TableHead>
                 <TableRow>
-                  <TableCell>Dessert (100g serving)</TableCell>
-                  <TableCell align="right">Calories</TableCell>
-                  <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                  <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                  <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                  <TableCell>Game</TableCell>
+                  <TableCell align="right">Platforms</TableCell>
+                  <TableCell align="right">Genre</TableCell>
+                  <TableCell align="right">Number of players</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
